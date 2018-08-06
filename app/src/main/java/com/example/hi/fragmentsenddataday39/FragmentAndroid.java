3fca634aec63815f84a697e8fd60be5d3926bcf1
Hi <mr.hoang.ht2009@gmail.com>
2018-08-06 18:15:39 +0700

    "commit"

--- app/src/main/java/com/example/hi/fragmentsenddataday39/FragmentAndroid.java
+++ app/src/main/java/com/example/hi/fragmentsenddataday39/FragmentAndroid.java
@@ -12,16 +12,25 @@ import android.view.ViewGroup;
 public class FragmentAndroid extends Fragment{
 
     View view;
+         //Neu ta muon chuyen 1 du lieu tu class nay qua class khac thi ta se tao ra 1 phuong thuc va sau do goi tu class can goi
+//    String chuoi = "";
+//    int number = 0;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_android,container,false);
-
-        Bundle bundle =getArguments();
-        String chuoi = bundle.getString("Chuoi");
-        Integer number = bundle.getInt("number");
-        Log.d("BBB",chuoi + ":" + number);
+            //Cach 1: Su dung Bundle khi Fragment chua duoc khoi tao
+//        Bundle bundle =getArguments();
+//        String chuoi = bundle.getString("Chuoi");
+//        Integer number = bundle.getInt("number");
+//        Log.d("BBB",chuoi + ":" + number);
         return view;
     }
+        //Dung phuong thuc SetData de chuyen du lieu
+//    public void SetData(String Chuoi, int so){
+//        chuoi = Chuoi;
+//        number = so;
+//        Log.d("BBB",chuoi + number);
+//    }
 }
--- app/src/main/java/com/example/hi/fragmentsenddataday39/MainActivity.java
+++ app/src/main/java/com/example/hi/fragmentsenddataday39/MainActivity.java
@@ -4,25 +4,48 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     FragmentManager fragmentManager = getSupportFragmentManager();
+    Button btnMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        FragmentAndroid fragmentAndroid = new FragmentAndroid();
+            //Cach 1: dung Bundle khi Fragment chua duoc khoi tao
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        FragmentAndroid fragmentAndroid = new FragmentAndroid();
+//
+//        //Tao Bundle
+//        Bundle bundle= new Bundle();
+//        bundle.putString("Chuoi","Chao Fragment Android");
+//        bundle.putInt("number",30);
+//
+//        fragmentAndroid.setArguments(bundle);
+//        fragmentTransaction.replace(R.id.relativeMain,fragmentAndroid);
+//        fragmentTransaction.commit();
 
-        //Tao Bundle
-        Bundle bundle= new Bundle();
-        bundle.putString("Chuoi","Chao Fragment Android");
-        bundle.putInt("number",30);
+            //Cach 2: Fragment da khoi tao roi
+        btnMain = findViewById(R.id.buttonMain);
 
-        fragmentAndroid.setArguments(bundle);
-        fragmentTransaction.replace(R.id.relativeMain,fragmentAndroid);
-        fragmentTransaction.commit();
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentAndroid fragmentAndroid = (FragmentAndroid) fragmentManager.findFragmentById(R.id.fragmentAndroid);
+                if (fragmentAndroid != null){
+                    TextView txtAndroid = fragmentAndroid.view.findViewById(R.id.textviewTitleAndroid);
+                    txtAndroid.setText("Main da setText cho Android");
+                }
+                    //chuyen du lieu tu class khac theo phuong thuc SetData
+//                if (fragmentAndroid != null){
+//                    fragmentAndroid.SetData("Chao Main",1);
+//                }
+            }
+        });
     }
 }
